1f3464be2c5de98ee399dbf9e1971d3d183a5c56
Computer wizard <chipwereexvin@gmail.com>
2026-05-15 06:11:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 const path = require('path');
 
 app.use(express.static(__dirname));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
